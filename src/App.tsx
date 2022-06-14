a7f79352febb7c41b66c749e854398a0874b9b9c
Matheus Santos <matheusps0012@gmail.com>
2022-06-14 18:31:25 -0300

    Add - Test For

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Component } from "solid-js";
+import { Component, createSignal, For } from "solid-js";
 
 import Header from "./components/Header";
 import Task from "./components/Task";
@@ -6,11 +6,18 @@ import Task from "./components/Task";
 import styles from "./App.module.scss";
 
 const App: Component = () => {
+  const [tasks, setTasks] = createSignal([
+    { id: 1, title: "" },
+    { id: 2, title: "" },
+  ]);
+
   return (
     <div class={styles.App}>
       <Header />
       <section>
-        <Task />
+        <For each={tasks()} fallback={<div>no items</div>}>
+          {(item, index) => <Task />}
+        </For>
       </section>
     </div>
   );
